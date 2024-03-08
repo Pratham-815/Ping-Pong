1c7c864d25e90d79439b140f6cab0bf2a555862a
Pratham Agarwal <prathamagarwal815@gmail.com>
2024-03-08 16:07:29 +0530

    Create two paddles by importing paddle class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from paddle import Paddle
 
 
 screen = Screen()
@@ -7,28 +8,15 @@ screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
 
+l_paddle = Paddle((-350, 0))
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_len=1, stretch_wid=5)
-paddle.penup()
-paddle.goto(x=350, y=0)
-
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
+r_paddle = Paddle((350, 0))
 
 screen.listen()
-screen.onkey(fun=go_up, key="Up")
-screen.onkey(fun=go_down, key="Down")
+screen.onkey(fun=r_paddle.go_up, key="Up")
+screen.onkey(fun=r_paddle.go_down, key="Down")
+screen.onkey(fun=l_paddle.go_up, key="w")
+screen.onkey(fun=l_paddle.go_down, key="s")
 
 game_is_on = True
 
